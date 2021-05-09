b52b0b552f616f0af29c70ecf9f9091e5e5b3ac6
Wojcik Michal <m.mwojcik@outlook.com>
2021-05-09 18:00:18 +0200

    initial commit

---
+++ apps/voting-dapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eth-angular-dapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'voting-dapp';
+}
